e0d6e993abbff5a7ce9eab4a3e1b5ffac2afc84f
ShaviyaVictor <shaviyavictor@gmail.com>
2023-06-20 16:35:32 -0500

    Create tests for the four scenarios when product is scanned and the quantity gets past the sku

--- src/tests/priceCheck.spec.js
+++ src/tests/priceCheck.spec.js
@@ -6,6 +6,15 @@ describe('check price calculations', function() {
     it('should scan product A and add its respective price of 50', function() {
       expect(calculate('A', 1)).toEqual(50);
     });
+    it('should scan 2 A products and total 100', function() {
+      expect(calculate('A', 2)).toEqual(100);
+    });
+    it('should scan 3 A products and total 150', function() {
+      expect(calculate('A', 3)).toEqual(150);
+    });
+    it('should scan 4 A products and total 200', function() {
+      expect(calculate('A', 4)).toEqual(200);
+    });
   });
 
 });
