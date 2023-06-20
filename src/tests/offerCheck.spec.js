b5a5e1f5a17ac42e8e7b6f8658efbbdb6470bc9c
ShaviyaVictor <shaviyavictor@gmail.com>
2023-06-20 18:56:21 -0500

    Create the test case for the addOffer() function

--- src/tests/offerCheck.spec.js
+++ src/tests/offerCheck.spec.js
@@ -2,6 +2,8 @@ import { addOffer, getOfferTotal } from "../logic/offerCalculator";
 
 describe('check offer calculations', function() {
 
-  
+  it('should allow you to add an offer', function() {
+    expect(addOffer).toBeTruthy();
+  });
 
 });
