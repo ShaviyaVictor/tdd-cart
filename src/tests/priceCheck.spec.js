2b63f9f7aa4a50e057458a52b75c3a27bc6cdbcf
ShaviyaVictor <shaviyavictor@gmail.com>
2023-06-20 16:15:37 -0500

    Create the 1st test that tests for product 1 after scanning

--- src/tests/priceCheck.spec.js
+++ src/tests/priceCheck.spec.js
@@ -2,6 +2,10 @@ import { calculate } from "../logic/prices";
 
 describe('check price calculations', function() {
 
-  
+  describe('A', function() {
+    it('should scan product A and add its respective price of 50', function() {
+      expect(calculate('A', 1)).toEqual(50);
+    });
+  });
 
 });
