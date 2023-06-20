2bd6759c19b1d310be37bfa8092f107d2696ee57
Victor Shaviya <vs610642@digicelgroup.com>
2023-06-20 15:17:48 -0500

    Create the addOffer and getOfferTotal functions

---
+++ src/logic/offerCalculator.js
@@ -0,0 +1,10 @@
+// sku - stock keeping units
+
+export function addOffer(sku, quantity, price) {
+  
+}
+
+export function getOfferTotal(sku, quantity) {
+  
+}
+
--- src/logic/prices.js
+++ src/logic/prices.js
@@ -7,3 +7,4 @@ const products = {
   D: 15
 };
 
+
