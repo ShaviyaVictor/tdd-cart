732b8e25ef4c855cfa06a21e1cb14fdfb8866af9
ShaviyaVictor <shaviyavictor@gmail.com>
2023-06-20 15:36:24 -0500

    Create the getOfferTotal function to calculate the total based on the sku

--- src/logic/offerCalculator.js
+++ src/logic/offerCalculator.js
@@ -4,10 +4,21 @@ export function addOffer(sku, quantity, price) {
   offers[sku] = {
     quantity: quantity,
     price: price
-  };
+  };  
 };
 
 export function getOfferTotal(sku, quantity) {
-  
+  let result = {
+    offerTotal: 0,
+    unmatchedItems: quantity
+  };
+
+  let skuOffer = offers[sku];
+
+  if(skuOffer && quantity >= skuOffer.quantity) {
+    let numberOfOfferMatches = Math.floor(quantity / skuOffer.quantity);
+    result.unmatchedItems = quantity % skuOffer.quantity;
+    result.offerTotal = skuOffer.price * numberOfOfferMatches;
+  }
 }
 
