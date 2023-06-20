8f629add33a6d206042387448858b9dc10557dc8
ShaviyaVictor <shaviyavictor@gmail.com>
2023-06-20 15:26:26 -0500

    Create the addOffer() function

--- src/logic/offerCalculator.js
+++ src/logic/offerCalculator.js
@@ -1,8 +1,11 @@
 // sku - stock keeping units
 
 export function addOffer(sku, quantity, price) {
-  
-}
+  offers[sku] = {
+    quantity: quantity,
+    price: price
+  };
+};
 
 export function getOfferTotal(sku, quantity) {
   
