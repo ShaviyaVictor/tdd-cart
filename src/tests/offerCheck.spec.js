03c16725acf8a71a9513c6b1924c8dba6ddb109f
ShaviyaVictor <shaviyavictor@gmail.com>
2023-06-20 18:58:14 -0500

    Create the test case for the getOfferTotal() function

--- src/tests/offerCheck.spec.js
+++ src/tests/offerCheck.spec.js
@@ -5,5 +5,10 @@ describe('check offer calculations', function() {
   it('should allow you to add an offer', function() {
     expect(addOffer).toBeTruthy();
   });
+  it('should allow you to get the offer total', function() {
+    expect(getOfferTotal).toBeTruthy();
+  });
+
+  
 
 });
