bb9298e2a795e266dd6d1bbfd97fe5af2f9a1eb8
ShaviyaVictor <shaviyavictor@gmail.com>
2023-06-20 19:20:43 -0500

    Create the test case for when there is a matching offer for product B

--- src/tests/offerCheck.spec.js
+++ src/tests/offerCheck.spec.js
@@ -11,7 +11,7 @@ describe('check offer calculations', function() {
 
   describe('when there are no offer', function() {
     it('should calculate offer price as 0 and remaining untotalled items as quantity passed in', function() {
-      // product A being 50 each and 130 for 3 under sku
+      // product A being 50 each and 130 for 3 under sku offer
       let result = getOfferTotal('A', 4);
       expect(result).toEqual({
         offerTotal: 0,
@@ -22,7 +22,7 @@ describe('check offer calculations', function() {
 
   describe('when there is a matching offer', function() {
     it('should calculate the offer price for A and pass in the remaining untotalled items', function() {
-      // product A being 50 each and 130 for 3 under sku
+      // product A being 50 each and 130 for 3 under sku offer
       addOffer('A', 3, 130);
       let result = getOfferTotal('A', 4);
       expect(result).toEqual({
@@ -30,6 +30,17 @@ describe('check offer calculations', function() {
         unmatchedItems: 1
       });
     });
-  })
+
+    it('should calculate offer price for product B and remaining untotalled items', function() {
+      // product B being 30 each and 45 for 2 under sku offer
+      addOffer('B', 2, 45);
+      let result = getOfferTotal('B', 3);
+      expect(result).toEqual({
+        offerTotal: 45,
+        unmatchedItems: 1
+      });
+    });
+  });
+
 
 });
