b28db89b602bfbdb15515132d9697931d8c61305
ShaviyaVictor <shaviyavictor@gmail.com>
2023-06-20 19:12:52 -0500

    Create the test case for when there is a matching offer

--- src/tests/offerCheck.spec.js
+++ src/tests/offerCheck.spec.js
@@ -18,6 +18,18 @@ describe('check offer calculations', function() {
         unmatchedItems: 4
       });
     });
+  });
+
+  describe('when there is a matching offer', function() {
+    it('should calculate the offer price for A and pass in the remaining untotalled items', function() {
+      // product A being 50 each and 130 for 3 under sku
+      addOffer('A', 3, 130);
+      let result = getOfferTotal('A', 4);
+      expect(result).toEqual({
+        offerTotal: 130,
+        unmatchedItems: 1
+      });
+    });
   })
 
 });
