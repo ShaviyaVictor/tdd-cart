61b6ca96acf5f9887948813e3b25b22833183fc1
ShaviyaVictor <shaviyavictor@gmail.com>
2023-06-21 21:13:11 -0500

    Create a failing test case that returns a total of zero if the cart is empty

--- src/tests/checkoutCheck.spec.js
+++ src/tests/checkoutCheck.spec.js
@@ -11,4 +11,8 @@ describe('the shopping cart component check', function() {
     expect(cart.total).toBeTruthy();
   });
 
+  it('it should have a total of zero if the cart is empty', function() {
+    expect(cart.total()).toEqual(0);
+  });
+
 });
