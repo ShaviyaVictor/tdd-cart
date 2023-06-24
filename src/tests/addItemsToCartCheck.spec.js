52aaebeb31ec3ddeb069972309455437cf3914e6
ShaviyaVictor <shaviyavictor@gmail.com>
2023-06-23 20:18:50 -0500

    Create the beforeEach function setup for testing

--- src/tests/addItemsToCartCheck.spec.js
+++ src/tests/addItemsToCartCheck.spec.js
@@ -1,7 +1,15 @@
+import { ShoppingCart } from "../logic/checkout";
+import { addOffer } from "../logic/offerCalculator";
 
 
 describe('add items to the shopping cart', function() {
 
-  
+  let cart;
+
+  beforeEach(function() {
+    addOffer('A', 3, 130);
+    addOffer('B', 2, 45);
+    cart = new ShoppingCart();
+  });
 
 });
